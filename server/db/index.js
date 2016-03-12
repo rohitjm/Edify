0f91e3255e6b5eaf88164e4ebac0237cc5d6525f
Thomas Hoffmann <thoff13@gmail.com>
2016-03-11 20:14:39 -0800

    Add mysql to back-end and start setting up User and Video tables;

---
+++ server/db/index.js
@@ -0,0 +1,20 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('thesis', 'root', 'Thomas');
+
+var User = db.define('User', {
+  username: Sequelize.STRING
+});
+
+var Video = db.define('Video', {
+  title: Sequelize.STRING,
+  description: Sequelize.STRING
+});
+
+Video.belongsTo(User);
+User.hasMany(Video);
+
+User.sync();
+Video.sync();
+
+exports.User = User;
+exports.Video = Video;
