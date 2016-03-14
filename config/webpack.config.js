9ec28d204a0182693268b775bc6ff7a99ddca9d9
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-03-14 11:15:59 -0700

    Implement Webpack config file

---
+++ config/webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+    path.resolve(__dirname, '../client/components/App.jsx')
+  ],
+  output: {
+    path: path.resolve(__dirname, '../public'),
+    publicPath: 'http://localhost:3000/',
+    filename: "bundle.js",
+  },
+  devtool: '#source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel']
+      },
+      {
+        test: /\.css$/, loader: "style-loader!css-loader"
+      }
+
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+}
