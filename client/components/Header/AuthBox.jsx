10504c85163dc45ace04cd4315c21e8ee0248924
Rohit Mathew <rohitjm@gmail.com>
2016-03-15 15:47:10 -0700

    Add AuthBox and SearchBar Components

---
+++ client/components/Header/AuthBox.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class AuthBox extends Component {
+
+  render(){
+  	return (
+  	  <div id = "header">
+		<p>Auth</p> 
+  	  </div>
+  	);
+  }
+}
--- client/containers/HeaderContainer.jsx
+++ client/containers/HeaderContainer.jsx
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+import AuthBox from '../components/Header/AuthBox.jsx';
+import SearchBar from '../components/Header/SearchBar.jsx';
 
 export default class Header extends Component {
 
   render(){
   	return (
   	  <div id = "header">
-		<p>Header</p> 
+		<AuthBox />
+		<SearchBar />
   	  </div>
   	);
   }
