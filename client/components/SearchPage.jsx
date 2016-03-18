1c8b290dc3017d6fd5c75f61260d4a215e5a735e
Hazal Ozcan <hazal.ozcan@yahoo.com>
2016-03-17 19:39:49 -0700

    Changes location of the window to Search Page

--- client/components/SearchPage.jsx
+++ client/components/SearchPage.jsx
@@ -1,16 +1,27 @@
 
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import VideoListEntry from './VideoListEntry.jsx';
+
 // import LogoBox from '../components/Header/LogoBox.jsx';
 // import AuthBox from '../components/Header/AuthBox.jsx';
 // import SearchBar from '../components/Header/SearchBar.jsx';
+ const mapStateToProps = (state) => {
+  return {
+    found: state.found
+  }
+};
+
 
 export default class SearchPage extends Component {
-
+ 
   render(){
     return (
-      <div id = "header">
+      <div id = "">
       Search Page!
       </div>
     );
   }
 }
+export default connect(mapStateToProps)(SearchPage);
+ // < VideoListEntry video = {this.props.found} />
