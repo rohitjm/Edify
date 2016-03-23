e960e452216ec44a354c279746b69bda5846f4a5
Thomas Hoffmann <thoff13@gmail.com>
2016-03-22 20:01:57 -0700

    Add test for reducer handling SHOW_SIGNIN_MODAL and HIDE_SIGNIN_MODAL actions;

--- test/reducersSpec.js
+++ test/reducersSpec.js
@@ -29,4 +29,21 @@ describe('reducer', () => {
     expect(nextState.user).to.deep.equal({name: 'The', password: 'Man'});
     expect(initialState.user).to.equal(undefined);
   });
+
+  it('handles SHOW_SIGNIN_MODAL and HIDE_SIGNIN_MODAL actions', () => {
+    const initialState_SHOW = {};
+    const action_SHOW = {type: 'SHOW_SIGNIN_MODAL'};
+    const nextState_SHOW = VideoAppHandler(initialState_SHOW, action_SHOW);
+
+    const initialState_HIDE = {};
+    const action_HIDE = {type: 'HIDE_SIGNIN_MODAL'};
+    const nextState_HIDE = VideoAppHandler(initialState_HIDE, action_HIDE);
+
+    expect(nextState_SHOW.displaySignInModal).to.deep.equal(true);
+    expect(initialState_SHOW.displaySignInModal).to.equal(undefined);
+
+    expect(nextState_HIDE.displaySignInModal).to.deep.equal(false);
+    expect(initialState_HIDE.displaySignInModal).to.equal(undefined);
+  });
+
 });
