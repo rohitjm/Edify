0c8ba9d5f89c8bf3d4c630eed323d7e962219c0e
Thomas Hoffmann <thoff13@gmail.com>
2016-03-23 20:19:52 -0700

    Clean up files; Switch DB to local; Add auth check to /addComment route;

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,7 +4,6 @@ var commentController = require('../controllers/commentController');
 var passport = require('./authentication').passport;
 var ensureAuthenticated = require('./authentication').ensureAuthenticated;
 
-console.log(videoController);
 module.exports = function(app, express) {
   // Handles request to sign up new user
   app.post('/signup', userController.userSignUp);
@@ -31,7 +30,7 @@ module.exports = function(app, express) {
   app.post('/loadComments', commentController.loadComments);
 
   // Add new comment to db
-  app.post('/addComment', commentController.addComment);
+  app.post('/addComment', ensureAuthenticated, commentController.addComment);
 
   //Handles adding about me on the profile page
   app.post('/aboutMe', userController.editAboutMe);
