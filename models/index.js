42848f9872d81a1829c70fc3411be33b917d886b
Hazal Ozcan <hazal.ozcan@yahoo.com>
2016-03-24 12:30:45 -0700

    Migration

--- models/index.js
+++ models/index.js
@@ -4,17 +4,16 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
+var env       = 'production' || process.env.NODE_ENV ;
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config)
+
 }
-
-
 
 fs
   .readdirSync(__dirname)
