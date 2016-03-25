af1f83aad775fb498ff92d4deda7d722f0de3188
Hazal Ozcan <hazal.ozcan@yahoo.com>
2016-03-24 17:05:58 -0700

    VoteController, implements UpVote/ DownVote

---
+++ server/controllers/voteController.js
@@ -0,0 +1,76 @@
+var db = require('../db');
+
+upVotes: function (req, res) {
+  var videoID = req.body.videoID;
+  var userID = req.body.userID;
+  db.Votes.findAll({where: {videoID: videoID, userID: userID}})
+  .then(function(vote) {
+    if(vote === undefined) {
+      db.Votes.create({videoID: videoID, userID: userID, upVotes: 1, downVotes:0})
+      .then(function() {
+        db.Votes.findAll({
+          attributes: [
+          [sequelize.fn('SUM', sequelize.col('upVotes')),'upVotes']
+          ],
+          where: {
+            videoID: videoID
+          }
+        }).then(function(voteCount) {
+          db.Videos.findAll({where: {videoID: videoID}})
+          .then(function(video) {
+            video.upVotes = voteCount
+            video.save()
+            .then(function(video){
+              res.sendStatus(201, video.voteCount);
+            })
+          })
+          .catch(function(err) {
+            throw err;
+            res.sendstatus(500);
+          });
+        })     
+      })
+    }
+  })
+  .catch(function(err) {
+    throw err;
+    res.status(500);
+  });
+},
+downVotes: function (req, res) {
+  var videoID = req.body.videoID;
+  var userID = req.body.userID;
+  db.Votes.findAll({where: {videoID: videoID, userID: userID}})
+  .then(function(vote) {
+    if(vote === undefined) {
+      db.Votes.create({videoID: videoID, userID: userID, upVotes: 0, downVotes:1})
+      .then(function() {
+        db.Votes.findAll({
+          attributes: [
+          [sequelize.fn('SUM', sequelize.col('downVotes')),'downVotes']
+          ],
+          where: {
+            videoID: videoID
+          }
+        }).then(function(voteCount) {
+          db.Videos.findAll({where: {videoID: videoID}})
+          .then(function(video) {
+            video.downVotes = voteCount
+            video.save()
+            .then(function(video){
+              res.sendStatus(201, video.voteCount);
+            })
+          })
+          .catch(function(err) {
+            throw err;
+            res.sendstatus(500);
+          });
+        })     
+      })
+    }
+  })
+  .catch(function(err) {
+    throw err;
+    res.status(500);
+  });
+},
