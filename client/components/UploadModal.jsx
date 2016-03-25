022d21c79e1f16b33ade1ecb57e543264c7a1edf
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-03-24 19:45:54 -0700

    Start adding categories to modal

--- client/components/UploadModal.jsx
+++ client/components/UploadModal.jsx
@@ -5,10 +5,12 @@ import TextField from 'material-ui/lib/text-field';
 import RaisedButton from 'material-ui/lib/raised-button';
 import FloatingActionButton from 'material-ui/lib/floating-action-button';
 import ContentAdd from 'material-ui/lib/svg-icons/content/add';
+import DropDownMenu from 'material-ui/lib/DropDownMenu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
 import {connect} from 'react-redux';
 import ReactS3Uploader from 'react-s3-uploader';
 
-import { addVideo, hideUploadModal } from '../actions/actions.jsx';
+import { addVideo, hideUploadModal, loadCategories } from '../actions/actions.jsx';
 
 export default class UploadModal extends React.Component {
   constructor(props) {
@@ -29,8 +31,19 @@ export default class UploadModal extends React.Component {
     // add snackbar for when video is finished uploading?
     // add progress bar while video is uploading?
 
-    var videoUrl;
-    var coverUrl;
+    let videoUrl;
+    let coverUrl;
+
+    let items;
+
+    // initialize drop down menu with categories
+    // if(!items) {
+    //   items = [];
+    //   this.props.fetchCategories();
+    //   for (let i = 0; i < this.props.categories.length; i++) {
+    //     items.push(<MenuItem value={i} key={i} primaryText={`${i}`}/>);
+    //   }
+    // }
 
     const actions = [
       <ReactS3Uploader  
@@ -55,7 +68,6 @@ export default class UploadModal extends React.Component {
       <FlatButton
         label='Submit'
         onClick={() => {
-          // video file name must equal the title for now
           console.log('submit button clicked')
           this.props.submitVideo({title: this.refs.title.getValue(), description: this.refs.description.getValue(), cover: coverUrl, user: this.props.user, url: videoUrl})
         }}
@@ -71,18 +83,20 @@ export default class UploadModal extends React.Component {
           contentStyle={customContentStyle}
           open={this.props.displayUploadModal === true}
         >
-        Select video (.mp4) and thumbnail (.jpg) files.
+          Select a video file (.mp4).
           <TextField
           ref="title"
           floatingLabelText="Title"
           id="title"
           />
+          Select a thumbnail file (.jpg).
           <TextField
             ref="description"
             floatingLabelText="Description"
             type="description"
             id="description"
           />
+          Category
         </Dialog>
       </div>
     );
@@ -92,7 +106,8 @@ export default class UploadModal extends React.Component {
 const mapStateToProps = (state) => {
   return {
     displayUploadModal: state.displayUploadModal,
-    user: state.user
+    user: state.user,
+    categories: state.categories
   }
 };
 
@@ -106,6 +121,9 @@ const mapDispatchToProps = (dispatch) => {
     },
     closeModal: () => {
       dispatch(hideUploadModal())
+    },
+    fetchCategories: () => {
+      dispatch(loadCategories())
     }
   };
 };
