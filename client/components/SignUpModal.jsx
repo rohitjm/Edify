aba17d655781e74607cd734e6b1c6d2ce0292604
Thomas Hoffmann <thoff13@gmail.com>
2016-03-30 11:48:37 -0700

    Clean up SignUpModal component;

--- client/components/SignUpModal.jsx
+++ client/components/SignUpModal.jsx
@@ -3,15 +3,22 @@ import Dialog from 'material-ui/lib/dialog';
 import FlatButton from 'material-ui/lib/flat-button';
 import TextField from 'material-ui/lib/text-field';
 import {connect} from 'react-redux';
+<<<<<<< HEAD
 import { signUpUser, hideSignUpModal, toggleSignUpModal, authError } from '../actions/actions.jsx';
+=======
+import { signUpUser, hideSignUpModal, toggleSignUpModal } from '../actions/actions.jsx';
+>>>>>>> Clean up SignUpModal component;
 
 export default class SignUpModal extends Component {
   render() {
     var signUp = this.props.signUp;
     var closeModal = this.props.closeModal;
     var displaySignUpModal = this.props.displaySignUpModal;
+<<<<<<< HEAD
     var authError = this.props.authError;
     var authErrorReset = this.props.authErrorReset;
+=======
+>>>>>>> Clean up SignUpModal component;
 
     const customContentStyle = {
       width: 350,
@@ -22,15 +29,23 @@ export default class SignUpModal extends Component {
       <FlatButton
         label='Cancel'
         secondary={true}
+<<<<<<< HEAD
         onClick={() => {
           closeModal();
           authErrorReset();
         }}
+=======
+        onClick={closeModal}
+>>>>>>> Clean up SignUpModal component;
       />,
       <FlatButton
         label='Submit'
         onClick={() => {
           signUp({username: this.refs.username.getValue(), password: this.refs.password.getValue()});
+<<<<<<< HEAD
+=======
+          closeModal();
+>>>>>>> Clean up SignUpModal component;
         }}
       />
     ];
@@ -66,8 +81,12 @@ export default class SignUpModal extends Component {
 
 const mapStateToProps = (state) => {
   return {
+<<<<<<< HEAD
     displaySignUpModal: state.displaySignUpModal,
     authError: state.authError
+=======
+    displaySignUpModal: state.displaySignUpModal
+>>>>>>> Clean up SignUpModal component;
   }
 };
 
