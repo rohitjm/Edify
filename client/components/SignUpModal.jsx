516ae9b9c46f7f30519a568888396c9e063382cf
Thomas Hoffmann <thoff13@gmail.com>
2016-03-30 11:48:37 -0700

    Clean up SignUpModal component;

--- client/components/SignUpModal.jsx
+++ client/components/SignUpModal.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Dialog from 'material-ui/lib/dialog';
 import FlatButton from 'material-ui/lib/flat-button';
 import TextField from 'material-ui/lib/text-field';
-import RaisedButton from 'material-ui/lib/raised-button';
 import {connect} from 'react-redux';
 import { signUpUser, hideSignUpModal, toggleSignUpModal } from '../actions/actions.jsx';
 
-export default class SignUpModal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
+export default class SignUpModal extends Component {
   render() {
+    var signUp = this.props.signUp;
+    var closeModal = this.props.closeModal;
+    var displaySignUpModal = this.props.displaySignUpModal;
 
     const customContentStyle = {
       width: 350,
@@ -22,12 +20,13 @@ export default class SignUpModal extends React.Component {
       <FlatButton
         label='Cancel'
         secondary={true}
-        onClick={this.props.closeModal}
+        onClick={closeModal}
       />,
       <FlatButton
         label='Submit'
         onClick={() => {
-          this.props.signUp({username: this.refs.username.getValue(), password: this.refs.password.getValue()})
+          signUp({username: this.refs.username.getValue(), password: this.refs.password.getValue()});
+          closeModal();
         }}
       />
     ];
@@ -39,7 +38,7 @@ export default class SignUpModal extends React.Component {
           actions={actions}
           modal={false}
           contentStyle={customContentStyle}
-          open={this.props.displaySignUpModal === true}
+          open={displaySignUpModal === true}
         >
           <TextField
           ref="username"
@@ -60,18 +59,14 @@ export default class SignUpModal extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    displaySignUpModal: state.displaySignUpModal,
-    user: state.user
+    displaySignUpModal: state.displaySignUpModal
   }
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     signUp: (user) => {
-      console.log("Signing up user", user)
-
       dispatch(signUpUser(user))
-      dispatch(hideSignUpModal())
     },
     closeModal: () => {
       dispatch(hideSignUpModal())
