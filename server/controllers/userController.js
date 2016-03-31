5124d7c35adcd09c2b634fe0c5d0c7786a5a273a
Thomas Hoffmann <thoff13@gmail.com>
2016-03-30 19:50:27 -0700

    Update userSignUp and userSignOut route handlers to handle creating and destroying session; Clean up error handling;

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,32 +7,30 @@ module.exports = {
   userSignUp: function (req, res) {
     db.User.findOne({where: {username: req.body.username}})
     .then(function(user) {
-      console.log("useris:", user);
       if (user) {
-        console.log('Username already exists!');
-        res.sendStatus(200);
+        res.sendStatus(200, {error: 'username-SignUp'}); 
       }
       bcrypt.hash(req.body.password, 10, function(err, hash) {
         if (err) {
-          res.send(err);
+          throw err;
+          res.sendStatus(500);
         }
         db.User.create({
           username: req.body.username,
           password: hash,
-
-
         })
         .then(function(user) {
-          console.log('User created successfully');
           res.send(201, user);
         })
         .catch(function(err) {
-          console.log(err);
+          throw err;
+          res.sendStatus(500);
         });
       });
     })
     .catch(function(err) {
-      console.log(err);
+      throw err;
+      res.sendStatus(500);
     });
   },
 
@@ -44,7 +42,7 @@ module.exports = {
     db.User.findOne({where: {username: req.body.username}})
     .then(function(user) {
       if (!user) {
-        res.sendStatus(401, {error: 'username'}); 
+        res.sendStatus(401, {error: 'username-SignIn'}); 
       }
       bcrypt.compare(password, user.password, function(err, match) {
         if (err) {
@@ -54,7 +52,11 @@ module.exports = {
         if (!match) {
           res.send(401, {error: 'password'}); 
         } else {
-          req.session.regenerate(function () {
+          req.session.regenerate(function (err) {
+            if (err) {
+              throw err;
+              res.sendStatus(500);
+            }
             req.session.userId = user.id;
             var usr = user;
             usr.password = "";
@@ -74,30 +76,32 @@ module.exports = {
 
   // Handles user signing out and removes their existing session
   userSignOut: function (req, res) {
-    // Built in function provided by Passport
-    req.logout();
-    console.log('User logged out!');
-    res.send('Logged out');
-    // Redirect or do something else to let user know they successfully loged out
+    req.session.destroy(function() {
+      if (err) {
+        throw err;
+        res.sendStatus(500);
+      }
+      res.send(200);
+    });
   },
 
 
   editAboutMe: function (req,res) {
     db.User.findOne({where: {username: req.body.username}})
     .then(function(user) {
-      console.log('User is:', user);
       user.aboutMe = req.body.info;
       user.save()
       .then(function(user) {
-        console.log('User updated successfully');
         res.send(201, user);
       })
       .catch(function(err) {
-        console.log(err);
+        throw err;
+        res.sendStatus(500);
       });
     })
     .catch(function(err) {
-      console.log(err);
+      throw err;
+      res.sendStatus(500);
     });
   }
 };
